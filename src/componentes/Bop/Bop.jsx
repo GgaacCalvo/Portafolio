7474aa2865e9589556f5f8d7025cf89c55c96f22
Guillermo Calvo <guillecalvo5@gmail.com>
2022-11-24 13:09:31 -0600

    fixes

--- src/componentes/Bop/Bop.jsx
+++ src/componentes/Bop/Bop.jsx
@@ -1,10 +1,14 @@
 import st from "./Top.module.css";
 import "../Skills/Skills.css";
+import { TbArrowBigTop } from "react-icons/tb";
+
 const Bop = () => {
   return (
     <div className={st.bop}>
-      <a className="text" id="back-to-top" href="#home">
-        ^
+      <a className="textfield" id="back-to-top" href="#home">
+        <div className={st.bop2}>
+          <TbArrowBigTop />
+        </div>
       </a>
     </div>
   );
