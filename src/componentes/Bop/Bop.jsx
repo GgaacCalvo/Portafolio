01cac0b9f74fdb0a3da51b32a76e57fd8a043389
Guillermo Calvo <guillecalvo5@gmail.com>
2022-12-12 16:17:43 -0600

    bug

--- src/componentes/Bop/Bop.jsx
+++ src/componentes/Bop/Bop.jsx
@@ -1,16 +1,22 @@
 import st from "./Top.module.css";
 import "../Skills/Skills.css";
 import { TbArrowBigTop } from "react-icons/tb";
+import { RiHome2Fill } from "react-icons/ri";
 
 const Bop = () => {
   return (
-    <div className={st.bop}>
-      <a className="textfield" id="back-to-top" href="#home">
-        <div className={st.bop2}>
-          <TbArrowBigTop />
-        </div>
-      </a>
-    </div>
+    <a className={st.b2} href="#home">
+      <div className={st.bop}>
+        <RiHome2Fill />
+      </div>
+    </a>
+    // <div className={st.bop}>
+    //   <a className="textfield" id="back-to-top" href="#home">
+    //     <div className={st.bop2}>
+    //       <TbArrowBigTop />
+    //     </div>
+    //   </a>
+    // </div>
   );
 };
 export default Bop;
--- src/componentes/NavBar.jsx
+++ src/componentes/NavBar.jsx
@@ -1,6 +1,18 @@
 import React from "react";
 import "./NavBar.css";
 import { BsLinkedin, BsGithub, BsInstagram } from "react-icons/bs";
+
+import { BsWhatsapp } from "react-icons/bs";
+
+import { motion } from "framer-motion";
+const iconAnimate = {
+  initial: {},
+  view: {
+    y: [0, -5, 0],
+    color: "#503010",
+    transition: { duration: 0.4 },
+  },
+};
 const NavBar = () => {
   return (
     <nav class="navg navbar navbar-expand-lg navbar-light ">
@@ -26,9 +38,9 @@ const NavBar = () => {
               Inicio <span class="sr-only">(current)</span>
             </a>
           </li>
-          <li class="nav-item active litem">
+          <li class="nav-item active litem ">
             <a class="nav-link" href="#about">
-              Sobre Mi <span class="sr-only">(current)</span>
+              Sobre Mi <span class="sr-only ">(current)</span>
             </a>
           </li>
           <li class="nav-item active litem">
@@ -45,13 +57,61 @@ const NavBar = () => {
       </div>
       <div className="social">
         <a
+          className="no-underline hover:underline"
+          href="https://www.linkedin.com/in/guillermo-calvo-790948192/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <motion.div
+            whileHover={"view"}
+            initial={{ color: "black" }}
+            variants={iconAnimate}
+            className="hidden mr-5 cursor-pointer md:block decoration-black"
+          >
+            <BsLinkedin size={20} />
+          </motion.div>
+        </a>
+        <a
+          href="https://github.com/GgaacCalvo"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <motion.div
+            initial={{ color: "black" }}
+            whileHover={"view"}
+            variants={iconAnimate}
+            className="hidden mr-5 cursor-pointer md:block"
+          >
+            <BsGithub size={20} />
+          </motion.div>
+        </a>
+        <a
+          href="https://www.instagram.com/guillecalvo5/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <motion.div
+            initial={{ color: "black" }}
+            whileHover={"view"}
+            variants={iconAnimate}
+            className="hidden mr-5 cursor-pointer md:block"
+          >
+            <BsInstagram size={20} />
+          </motion.div>
+        </a>
+        {/* <a
           className="social-item"
           href="https://www.linkedin.com/in/guillermo-calvo-790948192/"
           target="_blank"
           rel="noreferrer"
         >
-          {" "}
-          <BsLinkedin />{" "}
+          <motion.div
+          whileHover={"view"}
+          variants={iconAnimate}
+          className="hidden mr-5 cursor-pointer md:block no-underline"
+        >
+          <BsLinkedin size={20} />
+        </motion.div>
         </a>
         <a
           className="social-item"
@@ -59,8 +119,13 @@ const NavBar = () => {
           target="_blank"
           rel="noreferrer"
         >
-          {" "}
-          <BsGithub />{" "}
+          <motion.div
+          whileHover={"view"}
+          variants={iconAnimate}
+          className="hidden mr-5 cursor-pointer md:block"
+        >
+          <BsGithub size={20} />
+        </motion.div>
         </a>
         <a
           className="social-item"
@@ -68,9 +133,14 @@ const NavBar = () => {
           target="_blank"
           rel="noreferrer"
         >
-          {" "}
-          <BsInstagram />{" "}
-        </a>
+          <motion.div
+          whileHover={"view"}
+          variants={iconAnimate}
+          className="hidden mr-5 cursor-pointer md:block"
+        >
+          <BsInstagram size={20} />
+        </motion.div>
+        </a> */}
       </div>
     </nav>
   );
